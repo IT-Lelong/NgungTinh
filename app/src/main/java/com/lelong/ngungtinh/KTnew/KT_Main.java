ba4eb3f5e9ca1482cd965671bfc2c56e1fd8e77a
Nga <it_duchoa@mail.klb.com.tw>
2023-03-08 16:15:07 +0700

    add Chart

--- app/src/main/java/com/lelong/ngungtinh/KTnew/KT_Main.java
+++ app/src/main/java/com/lelong/ngungtinh/KTnew/KT_Main.java
@@ -2,46 +2,25 @@ package com.lelong.ngungtinh.KTnew;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.CombinedChart;
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.AxisBase;
-import com.github.mikephil.charting.components.Description;
-import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.CombinedData;
-import com.github.mikephil.charting.data.DataSet;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.formatter.IAxisValueFormatter;
-import com.github.mikephil.charting.highlight.Highlight;
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
-import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.lelong.ngungtinh.Create_Table;
 import com.lelong.ngungtinh.R;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class KT_Main extends AppCompatActivity {
     String g_xuong;
@@ -100,123 +79,28 @@ public class KT_Main extends AppCompatActivity {
         });
 
 
-        /*mChart = (PieChart) findViewById(R.id.piechart);
-        mChart.setRotationEnabled(true);
-        mChart.setDescription(new Description());
-        mChart.setHoleRadius(35f);
-        mChart.setTransparentCircleAlpha(0);
-        mChart.setCenterText("PieChart");
-        mChart.setCenterTextSize(10);
-        mChart.setDrawEntryLabels(true);
+        BarChart barChart = findViewById(R.id.barchart);
+        barChart.setDrawBarShadow(false);
 
-        addDataSet(mChart);
-        mChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
-            @Override
-            public void onValueSelected(Entry e, Highlight h) {
-                Toast.makeText(KT_Main.this,"Value" + e.getY() + ",index: " + h.getX() +", DataSetIndex: " + h.getDataSetIndex(), Toast.LENGTH_SHORT).show();
-            }
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+        barEntries.add(new BarEntry(1, 10));
+        barEntries.add(new BarEntry(2, 20));
+        barEntries.add(new BarEntry(3, 30));
+        barEntries.add(new BarEntry(4, 40));
 
-            @Override
-            public void onNothingSelected() {
+        ArrayList<String> labels = new ArrayList<>();
+        labels.add("Column 1");
+        labels.add("Column 2");
+        labels.add("Column 3");
+        labels.add("Column 4");
 
-            }
-        });*/
-        BarChart chart = findViewById(R.id.barchart);
+        BarDataSet barDataSet = new BarDataSet(barEntries, "Columns");
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
 
-        ArrayList NoOfEmp = new ArrayList();
-
-        NoOfEmp.add(new BarEntry(945f, 0));
-        NoOfEmp.add(new BarEntry(1040f, 1));
-        NoOfEmp.add(new BarEntry(1133f, 2));
-        NoOfEmp.add(new BarEntry(1240f, 3));
-        NoOfEmp.add(new BarEntry(1369f, 4));
-        NoOfEmp.add(new BarEntry(1487f, 5));
-        NoOfEmp.add(new BarEntry(1501f, 6));
-        NoOfEmp.add(new BarEntry(1645f, 7));
-        NoOfEmp.add(new BarEntry(1578f, 8));
-        NoOfEmp.add(new BarEntry(1695f, 9));
-
-        BarDataSet bardataset = new BarDataSet(NoOfEmp, "No Of Employee");
-
-        ArrayList<String> year = new ArrayList<String>();
-
-        year.add("2008");
-        year.add("2009");
-        year.add("2010");
-        year.add("2011");
-        year.add("2012");
-        year.add("2013");
-        year.add("2014");
-        year.add("2015");
-        year.add("2016");
-        year.add("2017");
-
-        //BarDataSet bardataset = new BarDataSet(NoOfEmp, "No Of Employee");
-        chart.animateY(5000);
-        //BarData data = new BarData(year, bardataset);
-         chart = new BarChart(this);
-        /*setContentView(chart);
-        BarChart data =new BarChart(year,bardataset);
-        bardataset.setColors(ColorTemplate.COLORFUL_COLORS);
-        chart.setData(data);*/
+        BarData barData = new BarData(barDataSet);
+        barData.setBarWidth(0.9f);
+        barChart.setData(barData);
+        barChart.invalidate();
     }
-    /*private static void addDataSet(PieChart pieChart) {
-        ArrayList<PieEntry> yEntrys = new ArrayList<>();
-        ArrayList<String> xEntrys = new ArrayList<>();
-        float[] yData = { 25, 40, 70 };
-        String[] xData = { "A", "B", "C" };
 
-        for (int i = 0; i < yData.length;i++){
-            yEntrys.add(new PieEntry(yData[i],i));
-        }
-        for (int i = 0; i < xData.length;i++){
-            xEntrys.add(xData[i]);
-        }
-
-        PieDataSet pieDataSet=new PieDataSet(yEntrys,"Employee Sales");
-        pieDataSet.setSliceSpace(2);
-        pieDataSet.setValueTextSize(12);
-
-        ArrayList<Integer> colors=new ArrayList<>();
-        colors.add(Color.GRAY);
-        colors.add(Color.BLUE);
-        colors.add(Color.RED);
-
-        pieDataSet.setColors(colors);
-
-        Legend legend=pieChart.getLegend();
-        legend.setForm(Legend.LegendForm.CIRCLE);
-        legend.setPosition(Legend.LegendPosition.LEFT_OF_CHART);
-
-        PieData pieData=new PieData(pieDataSet);
-        pieChart.setData(pieData);
-        pieChart.invalidate();
-    }*/
-    private static DataSet dataChart() {
-
-        LineData d = new LineData();
-        int[] data = new int[] { 1, 2, 2, 1, 1, 1, 2, 1, 1, 2, 1, 9 };
-
-        ArrayList<Entry> entries = new ArrayList<Entry>();
-
-        for (int index = 0; index < 12; index++) {
-            entries.add(new Entry(index, data[index]));
-        }
-
-        LineDataSet set = new LineDataSet(entries, "Request Ots approved");
-        set.setColor(Color.GREEN);
-        set.setLineWidth(2.5f);
-        set.setCircleColor(Color.GREEN);
-        set.setCircleRadius(5f);
-        set.setFillColor(Color.GREEN);
-        set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
-        set.setDrawValues(true);
-        set.setValueTextSize(10f);
-        set.setValueTextColor(Color.GREEN);
-
-        set.setAxisDependency(YAxis.AxisDependency.LEFT);
-        d.addDataSet(set);
-
-        return set;
-    }
 }
